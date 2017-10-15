f262647814674185bd6cdbb364db6672119c5ad2
陈顺 <13786285072@163.com>
2017-10-15 10:28:16 +0800

    Create RealSubject.java

---
+++ src/com/cglib/RealSubject.java
@@ -0,0 +1,10 @@
+package top.cglib;
+
+/**
+ * Created by 竹 on 2017/10/15.
+ */
+public class RealSubject implements ISubjectService {
+    public void doService() {
+        System.out.println("doService ing。。。。");
+    }
+}
