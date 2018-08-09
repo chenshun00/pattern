9e5c2a107a3120a650259a227c3ebd6f23b8105d
chenshun00 <13786285072@163.com>
2018-08-09 09:32:44 +0800

    :sparkles: 添加builder模式

---
+++ src/main/java/com/builder/App.java
@@ -0,0 +1,28 @@
+package com.builder;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Date;
+
+/**
+ * builder pattern，我们能使用lombok的@Builder去简化
+ *  ⚠️:最后的build是直接通过构造方法返回的，所以需要添加默认的构造方法，防止使用mybatis等orm的时候反射构建对象失败
+ * @author luobo.cs@raycloud.com
+ * @since 2018/8/9
+ */
+@Slf4j
+public class App {
+    public static void main(String[] args) {
+        log.debug("builder pattern:{}", User.builder()
+                .userId(1)
+                .username("chenshun")
+                .nick("chenshun00")
+                .email("chenshun00@gamil.com")
+                .integral(1111)
+                .birthday(new Date())
+                .lastLogin(new Date())
+                .loginCount(89)
+                .phone("1111111")
+                .build());
+    }
+}
---
+++ src/main/java/com/builder/User.java
@@ -0,0 +1,213 @@
+package com.builder;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * 一个简单的builder，在很多field的情况下会提高可阅读性，不适合参数较少的情况
+ *
+ * @author luobo.cs@raycloud.com
+ * @since 2018/8/9
+ */
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3031056354692884466L;
+
+    private Integer userId;
+    private String username;
+    private String nick;
+    private String email;
+    private String phone;
+    private Date birthday;
+    private Date register;
+    private Date lastLogin;
+    private Integer integral;
+    private Integer loginCount;
+
+
+    public User() {
+    }
+
+    public User(Integer userId, String username, String nick, String email, String phone, Date birthday, Date register, Date lastLogin, Integer integral, Integer loginCount) {
+        this.userId = userId;
+        this.username = username;
+        this.nick = nick;
+        this.email = email;
+        this.phone = phone;
+        this.birthday = birthday;
+        this.register = register;
+        this.lastLogin = lastLogin;
+        this.integral = integral;
+        this.loginCount = loginCount;
+    }
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
+
+    public static class UserBuilder {
+
+        private static Integer userId;
+        private static String username;
+        private String nick;
+        private String email;
+        private String phone;
+        private Date birthday;
+        private Date register;
+        private Date lastLogin;
+        private Integer integral;
+        private Integer loginCount;
+
+        public  User build() {
+            return new User(this.userId, this.username, this.nick, this.email, this.phone, this.birthday, this.register, this.lastLogin, this.integral, this.loginCount);
+        }
+
+        public UserBuilder birthday(Date birthday) {
+            this.birthday = birthday;
+            return this;
+        }
+
+        public UserBuilder register(Date register) {
+            this.register = register;
+            return this;
+        }
+
+        public UserBuilder lastLogin(Date lastLogin) {
+            this.lastLogin = lastLogin;
+            return this;
+        }
+
+        public UserBuilder integral(Integer integral) {
+            this.integral = integral;
+            return this;
+        }
+
+        public UserBuilder loginCount(Integer loginCount) {
+            this.loginCount = loginCount;
+            return this;
+        }
+
+
+        public UserBuilder userId(Integer userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder nick(String nick) {
+            this.nick = nick;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+    }
+
+    //getter and setter
+    public  Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Date getRegister() {
+        return register;
+    }
+
+    public void setRegister(Date register) {
+        this.register = register;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public Integer getIntegral() {
+        return integral;
+    }
+
+    public void setIntegral(Integer integral) {
+        this.integral = integral;
+    }
+
+    public Integer getLoginCount() {
+        return loginCount;
+    }
+
+    public void setLoginCount(Integer loginCount) {
+        this.loginCount = loginCount;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", nick='" + nick + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthday=" + birthday +
+                ", register=" + register +
+                ", lastLogin=" + lastLogin +
+                ", integral=" + integral +
+                ", loginCount=" + loginCount +
+                '}';
+    }
+}
