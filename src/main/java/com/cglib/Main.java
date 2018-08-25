e631ad8c7c036572ff52daf3a7222d67e6948b9f
chenshun00 <13786285072@163.com>
2018-08-25 23:22:31 +0800

    :building_construction: 调整enhancer的参数，更加灵活

--- src/main/java/com/cglib/Main.java
+++ src/main/java/com/cglib/Main.java
@@ -1,16 +1,54 @@
 package com.cglib;
 
-import net.sf.cglib.proxy.Enhancer;
-import net.sf.cglib.proxy.MethodInterceptor;
+import net.sf.cglib.proxy.*;
+
+import java.lang.reflect.Method;
 
 /**
  * @author 竹
- *         on 2018/4/15.
+ * on 2018/4/15.
  */
 public class Main {
-        public static void main(String[] args) {
-            ISubjectService o = (ISubjectService) Enhancer.create(RealSubject.class, (MethodInterceptor) (obj, method, args1, proxy) ->
-                    proxy.invokeSuper(obj, args1));
-            o.doService();
+    public static void main(String[] args) {
+        Enhancer enhancer = new Enhancer();
+        enhancer.setSuperclass(RealSubject.class);
+        enhancer.setNamingPolicy(new MyDefaultNamingPolicy());
+//        enhancer.setCallback((MethodInterceptor) (obj, method, args1, proxy) -> {
+//            System.out.println("前边");
+//            try {
+//                return proxy.invokeSuper(obj, args1);
+//            } finally {
+//                System.out.println("后边");
+//            }
+//        });
+        //enhancer.setCallback(new DCallback()) 只是一个特殊的 enhancer.setCallbacks ，其实内部存在一个callbacks的数组
+        //之后的调用会按照setCallbackFilter的过滤去选择callbacks数组下标中的一个，cglib提供了默认的实现，默认是0
+        Callback[] callbacks = new Callback[]{new First(), new Second()};
+        enhancer.setCallbacks(callbacks);
+        //按照具体的情况调用哪一个拦截器
+        enhancer.setCallbackFilter(method -> {
+            //如果是1那么调用第二个，这里是按照下标去算的，如果
+            return 0;
+        });
+        RealSubject realSubject = (RealSubject) enhancer.create();
+        System.out.println(realSubject.getClass().getName());
+        realSubject.doService();
     }
+
+    private static class First implements MethodInterceptor {
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+            System.out.println("first");
+            return proxy.invokeSuper(obj, args);
+        }
+    }
+
+    private static class Second implements MethodInterceptor {
+        @Override
+        public Object intercept(Object obj, Method method, Object[] args, MethodProxy proxy) throws Throwable {
+            System.out.println("Second");
+            return proxy.invokeSuper(obj, args);
+        }
+    }
+
 }
